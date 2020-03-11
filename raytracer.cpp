8cd62c54a4052976d3501bde2df0eadb4829b4f3
Darya <tannenwald@inbox.ru>
2020-03-11 13:05:44 -0700

    -out flag (ver2)

--- raytracer.cpp
+++ raytracer.cpp
@@ -2,33 +2,11 @@
 #include "raytracing/raytracing.h"
 
 int main(int argc, const char** argv) 
-{
-  std::unordered_map<std::string, std::string> cmdLineParams;
-
-  for(int i=0; i<argc; i++)
-  {
-    std::string key(argv[i]);
-
-    if(key.size() > 0 && key[0]=='-')
-    {
-      if(i != argc-1) // not last argument
-      {
-        cmdLineParams[key] = argv[i+1];
-        i++;
-      }
-      else
-        cmdLineParams[key] = "";
-    }
-  }
-
-std::string outFilePath = "out.ppm";
-  if(cmdLineParams.find("-out") != cmdLineParams.end())
-    outFilePath = cmdLineParams["-out"];
-    
+{    
   try {
-      picture::Picture my_pic;
-      raytracing::render(outFilePath.c_str(), my_pic.spheres, my_pic.lights, my_pic.cubes);
-      std::cout << "result is saved in build directory in file " << outFilePath.c_str() << std::endl;
+      picture::Picture my_pic (argc, argv);
+      raytracing::render(my_pic.out_file_path.c_str(), my_pic.spheres, my_pic.lights, my_pic.cubes);
+      std::cout << "result is saved in build directory in file " << my_pic.out_file_path.c_str() << std::endl;
   }
   catch (const std::exception& er){
       std::cout << er.what();
--- raytracing/raytracing.h
+++ raytracing/raytracing.h
@@ -9,7 +9,7 @@ namespace raytracing {
 ///          picture,
 /// \param spheres - vector of spheres
 /// \param lights - vector of lights
-void render(const char* outFilePath,
+void render(const char* out_file_path,
             const std::vector<entities::Sphere> &spheres,
             const std::vector<entities::Light> &lights,
             const std::vector<entities::Cube> &cubes);
