15205a6c4c80843be0d6f2bcc234e981ff5d60bb
Victor Zadyabin <41433372+VityasZV@users.noreply.github.com>
2020-03-13 01:38:07 +0300

    Ieararchy modified - now all figures processed the same (#10)

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -31,40 +31,20 @@ Vec3f refract(const Vec3f &I, const Vec3f &N, const float eta_t, const float eta
     return k<0 ? Vec3f(1,0,0) : I*eta + N*(eta*cosi - sqrtf(k)); // k<0 = total reflection, no ray to refract. I refract it anyways, this has no physical meaning
 }
 
-bool scene_intersect(const raytracing::entities::Ray& ray, const std::vector<raytracing::entities::Sphere> &spheres,
-                     const std::vector<raytracing::entities::Cube> &cubes, const std::vector<raytracing::entities::Triangle> &triangles,
+bool scene_intersect(const raytracing::entities::Ray& ray, const std::vector<std::unique_ptr<const raytracing::entities::Figure>> &figures,
                      Vec3f &hit, Vec3f &N, raytracing::entities::Material &material) {
     float spheres_dist = std::numeric_limits<float>::max();
-    for (size_t i=0; i < spheres.size(); i++) {
-        float dist_i;
-        if (spheres[i].ray_intersect(ray, dist_i) && dist_i < spheres_dist) {
-            spheres_dist = dist_i;
-            hit = ray.orig + ray.dir*dist_i;
-            N = (hit - spheres[i].center).normalize();
-            material = spheres[i].material;
-        }
-    }
-
     float triangles_dist = std::numeric_limits<float>::max();
-    for (size_t i = 0; i < triangles.size(); ++i){
-        float dist_i;
-        if (triangles[i].ray_intersect(ray, dist_i) && dist_i < triangles_dist){
-            triangles_dist = dist_i;
-            hit = ray.orig + ray.dir*dist_i;
-            N = triangles[i].get_N();
-            material = triangles[i].material;
-        }
-    }
-
     float cubes_dist = std::numeric_limits<float>::max();
-    for (size_t i=0; i < cubes.size(); i++) {
+
+    for (const auto& p : figures){
         float dist_i;
-        if (cubes[i].ray_intersect(ray, dist_i) && dist_i < cubes_dist) {
-            cubes_dist = dist_i;
-            hit = ray.orig + ray.dir * dist_i;
-            // TODO вот от N зависит цвет проекции походу каждой
-            N = (hit - cubes[i].bounds[0]).normalize();
-            material = cubes[i].material;
+        if (p->ray_intersect(ray, dist_i)){
+            auto &figure_dist = p->NeededDist(spheres_dist, triangles_dist, cubes_dist);
+            if (dist_i < figure_dist){
+                figure_dist = dist_i;
+                p->SetNeededNormHitMaterial(ray, dist_i, N, hit, material);
+            }
         }
     }
 
@@ -88,14 +68,12 @@ namespace raytracing {
 namespace entities{
 
     Vec3f casting_ray::cast_ray(const Ray &ray,
-                                const std::vector<Sphere> &spheres,
-                                const std::vector<Light> &lights,
-                                const std::vector<entities::Cube> &cubes,
-                                const std::vector<entities::Triangle> &triangles, size_t depth) {
+                                const std::vector<std::unique_ptr<const entities::Figure>> &figures,
+                                const std::vector<Light> &lights, size_t depth) {
         Vec3f point, N;
         Material material;
 
-        if (depth>4 || !scene_intersect(ray, spheres, cubes, triangles, point, N, material)) {
+        if (depth>4 || !scene_intersect(ray, figures, point, N, material)) {
             return Vec3f(0, float(127.0/255), float(255.0/255)); // background color
         }
 
@@ -103,9 +81,9 @@ namespace entities{
         Vec3f reflect_dir = reflect(ray.dir, N).normalize();
         Vec3f refract_dir = refract(ray.dir, N, material.refractive_index).normalize();
         Ray reflect_ray(reflect_dir*N < 0 ? point - N*1e-3 : point + N*1e-3,
-                                              reflect_dir, &spheres, &lights, &cubes, &triangles, depth+1);// offset the original point to avoid occlusion by the object itself (in first param)
+                                              reflect_dir, &figures, &lights, depth+1);// offset the original point to avoid occlusion by the object itself (in first param)
         Ray refract_ray(refract_dir*N < 0 ? point - N*1e-3 : point + N*1e-3,
-                                              refract_dir, &spheres, &lights, &cubes, &triangles, depth + 1);
+                                              refract_dir, &figures, &lights, depth + 1);
 
         float diffuse_light_intensity = 0, specular_light_intensity = 0;
         for (size_t i=0; i<lights.size(); i++) {
@@ -115,7 +93,7 @@ namespace entities{
             Vec3f shadow_orig = light_dir*N < 0 ? point - N*1e-3 : point + N*1e-3; // checking if the point lies in the shadow of the lights[i]
             Vec3f shadow_pt, shadow_N;
             Material tmp_material;
-            if (scene_intersect(Ray(shadow_orig, light_dir), spheres, cubes, triangles,
+            if (scene_intersect(Ray(shadow_orig, light_dir), figures,
                                 shadow_pt, shadow_N, tmp_material) &&
                 (shadow_pt - shadow_orig).norm() < light_distance)
                 continue;
@@ -217,13 +195,12 @@ namespace entities{
 
 
 
-void render(const char* out_file_path, const std::vector<entities::Sphere> &spheres, const std::vector<entities::Light> &lights,
-            const std::vector<entities::Cube> &cubes, const std::vector<entities::Triangle> &triangles) {
+void render(const char* out_file_path, const std::vector<std::unique_ptr<const entities::Figure>> &figures, const std::vector<entities::Light> &lights) {
     const int   width    = 1920;
     const int   height   = 1080;
     const float fov      = M_PI/3.; ///that's a viewing angle = pi/3
     std::vector<Vec3f> framebuffer(width*height);
-    const auto amount_of_threads = 1;//std::thread::hardware_concurrency();
+    const auto amount_of_threads = std::thread::hardware_concurrency();
     std::vector<std::future<void>> tasks(amount_of_threads);
 
     size_t portion = width / amount_of_threads;
@@ -232,14 +209,13 @@ void render(const char* out_file_path, const std::vector<entities::Sphere> &sphe
         for (size_t start = 0, finish = portion, index = 0;
         index < amount_of_threads; ++index, start=finish,
         finish = index == amount_of_threads - 1 ? width : finish + portion){
-            tasks[index] = std::async(std::launch::async, [=, &spheres, &lights, &cubes, &triangles, &framebuffer](){
+            tasks[index] = std::async(std::launch::async, [=, &figures, &framebuffer](){
                 for (size_t i = start; i < finish; i++) {
                     auto dir_x = (i + 0.5) - width / 2.;
                     auto dir_y = -(j + 0.5) + height / 2.;    // this flips the image at the same time
                     auto dir_z = -height / (2. * tan(fov / 2.));
                     framebuffer[i + j * width] = entities::casting_ray::cast_ray(entities::Ray(
-                            Vec3f(0, 0, 0), Vec3f(dir_x, dir_y, dir_z).normalize()),
-                                                          spheres, lights, cubes, triangles);
+                            Vec3f(0, 0, 0), Vec3f(dir_x, dir_y, dir_z).normalize()), figures, lights);
                 }
             });
         }
--- raytracing/raytracing.h
+++ raytracing/raytracing.h
@@ -10,10 +10,8 @@ namespace raytracing {
 /// \param spheres - vector of spheres
 /// \param lights - vector of lights
 void render(const char* out_file_path,
-            const std::vector<entities::Sphere> &spheres,
-            const std::vector<entities::Light> &lights,
-            const std::vector<entities::Cube> &cubes,
-            const std::vector<entities::Triangle> &triangles);
+            const std::vector<std::unique_ptr<const entities::Figure>> &figures,
+            const std::vector<entities::Light> &lights);
 
 }//namespace raytracing
 
