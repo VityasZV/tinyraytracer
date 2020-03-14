4a4c664ae1b6c5f03eb5e85f8651394bf94ea0f4
Victor Zadyabin <41433372+VityasZV@users.noreply.github.com>
2020-03-14 14:13:36 +0300

    clang-format and duck of triangles (#11)

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,23 +1,26 @@
-#include "picture.h"
+#include "picture/picture.h"
 #include "raytracing/raytracing.h"
 
-int main(int argc, const char** argv) 
-{    
-  try {
-      picture::Picture my_pic (argc, argv);
-      switch  (my_pic.scene_id) {
-          case 1: raytracing::render(my_pic.out_file_path.c_str(), my_pic.figures, my_pic.lights);
-                  std::cout << "result is saved in build directory in file " << my_pic.out_file_path.c_str() << std::endl;
-                  break;
-          case 2: return 0;
-          case 3: return 0;
-          default: throw std::runtime_error ("Incorrect scene number!");
-      }
-      
-  }
-  catch (const std::exception& er){
-      std::cout << er.what();
-  }
-  return 0;
+int main(int argc, const char **argv) {
+    try {
+        picture::Picture my_pic(argc, argv);
+        switch (my_pic.scene_id) {
+            case 1:
+                raytracing::render(my_pic.out_file_path.c_str(), my_pic.figures, my_pic.lights);
+                std::cout << "result is saved in build directory in file " << my_pic.out_file_path.c_str() << std::endl;
+                break;
+            case 2:
+                return 0;
+            case 3:
+                return 0;
+            default:
+                throw std::runtime_error("Incorrect scene number!");
+        }
+
+    }
+    catch (const std::exception &er) {
+        std::cout << er.what();
+    }
+    return 0;
 }
 
