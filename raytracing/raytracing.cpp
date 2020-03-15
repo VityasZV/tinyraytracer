79e641134cb3250a88acf19aa8c78c4013eea58c
Владимир <shaposh-vova@rambler.ru>
2020-03-15 23:43:07 +0300

    Some env inform added

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -250,14 +250,15 @@ void render(const char *out_file_path, const std::vector<std::unique_ptr<const e
 //    ofs.open(out_file_path, std::ios::binary);
 //    ofs << "P6\n" << width << " " << height << "\n255\n";
 
-    for (size_t i = 0; i < height * width; ++i) {
-        Vec3f &c = framebuffer[i];
-        float max = std::max(c[0], std::max(c[1], c[2]));
-        if (max > 1) c = c * (1. / max);
-        for (size_t j = 0; j < 3; ++j) {
-            ofs << (char) (255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
-    }
+//    for (size_t i = 0; i < height * width; ++i) {
+//        Vec3f &c = framebuffer[i];
+//        float max = std::max(c[0], std::max(c[1], c[2]));
+//        if (max > 1) c = c * (1. / max);
+//        for (size_t j = 0; j < 3; ++j) {
+//            ofs << (char) (255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+//        }
+//    }
+    stbi_write_jpg("out.jpg", width-delta, height, 3, pixmap.data(), 100);
 //    ofs.close();
 }
 
